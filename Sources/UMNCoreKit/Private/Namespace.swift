da2790fdf047aa07d6415c431687a0a0031a8d49
Oscar Byström Ericsson <oscbyspro@protonmail.com>
2023-12-24 12:05:33 +0100

    [CoreKit] Namespace.

---
+++ Sources/UMNCoreKit/Private/Namespace.swift
@@ -0,0 +1,26 @@
+//=----------------------------------------------------------------------------=
+// This source file is part of the Ultimathnum open source project.
+//
+// Copyright (c) 2023 Oscar Byström Ericsson
+// Licensed under Apache License, Version 2.0
+//
+// See http://www.apache.org/licenses/LICENSE-2.0 for license information.
+//=----------------------------------------------------------------------------=
+
+//*============================================================================*
+// MARK: * UMN x Namespace
+//*============================================================================*
+
+/// A namespace for `Ultimathnum` development.
+///
+/// - Warning: Do not use this namespace outside of `Ultimathnum` development.
+///
+@frozen public enum UMN {
+    
+    //=------------------------------------------------------------------------=
+    // MARK: Aliases
+    //=------------------------------------------------------------------------=
+    
+    /// The sign of a numeric value.
+    public typealias Sign = FloatingPointSign
+}
