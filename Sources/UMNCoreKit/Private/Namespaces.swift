f54e364814f795777412332c3040cd6279995121
Oscar Byström Ericsson <oscbyspro@protonmail.com>
2024-01-03 10:40:48 +0100

    [CoreKit] Namespaces.

---
+++ Sources/UMNCoreKit/Private/Namespaces.swift
@@ -0,0 +1,65 @@
+//=----------------------------------------------------------------------------=
+// This source file is part of the Ultimathnum open source project.
+//
+// Copyright (c) 2023 Oscar Byström Ericsson
+// Licensed under Apache License, Version 2.0
+//
+// See http://www.apache.org/licenses/LICENSE-2.0 for license information.
+//=----------------------------------------------------------------------------=
+
+//*============================================================================*
+// MARK: * UMN x Namespaces
+//*============================================================================*
+
+/// A namespace for `Ultimathnum` development.
+///
+/// - Warning: Do not use this namespace outside of `Ultimathnum`.
+///
+@frozen public enum UMN {
+    
+    //=------------------------------------------------------------------------=
+    // MARK: Namespaces x Binary Integer
+    //=------------------------------------------------------------------------=
+    
+    /// A namespace for `Ultimathnum` development.
+    ///
+    /// - Warning: Do not use this namespace outside of `Ultimathnum`.
+    ///
+    public typealias SBI<Base> = UMN.StrictBinaryInteger<Base>
+    where Base: RandomAccessCollection, Base.Element: UMNSystemInteger & UMNUnsignedInteger
+
+    /// A namespace for `Ultimathnum` development.
+    ///
+    /// - Warning: Do not use this namespace outside of `Ultimathnum`.
+    ///
+    public typealias SBISS<Base> = UMN.StrictBinaryInteger<Base>.SubSequence
+    where Base: RandomAccessCollection, Base.Element: UMNSystemInteger & UMNUnsignedInteger
+    
+    /// A namespace for `Ultimathnum` development.
+    ///
+    /// - Warning: Do not use this namespace outside of `Ultimathnum`.
+    ///
+    public typealias SSI<Base> = UMN.StrictSignedInteger<Base>
+    where Base: RandomAccessCollection, Base.Element: UMNSystemInteger & UMNUnsignedInteger
+
+    /// A namespace for `Ultimathnum` development.
+    ///
+    /// - Warning: Do not use this namespace outside of `Ultimathnum`.
+    ///
+    public typealias SSISS<Base> = UMN.StrictSignedInteger<Base>.SubSequence
+    where Base: RandomAccessCollection, Base.Element: UMNSystemInteger & UMNUnsignedInteger
+
+    /// A namespace for `Ultimathnum` development.
+    ///
+    /// - Warning: Do not use this namespace outside of `Ultimathnum`.
+    ///
+    public typealias SUI<Base> = UMN.StrictUnsignedInteger<Base>
+    where Base: RandomAccessCollection, Base.Element: UMNSystemInteger & UMNUnsignedInteger
+
+    /// A namespace for `Ultimathnum` development.
+    ///
+    /// - Warning: Do not use this namespace outside of `Ultimathnum`.
+    ///
+    public typealias SUISS<Base> = UMN.StrictUnsignedInteger<Base>.SubSequence
+    where Base: RandomAccessCollection, Base.Element: UMNSystemInteger & UMNUnsignedInteger
+}
---
+++ Sources/UMNCoreKit/Private/SUI.swift
@@ -0,0 +1,53 @@
+//=----------------------------------------------------------------------------=
+// This source file is part of the Ultimathnum open source project.
+//
+// Copyright (c) 2023 Oscar Byström Ericsson
+// Licensed under Apache License, Version 2.0
+//
+// See http://www.apache.org/licenses/LICENSE-2.0 for license information.
+//=----------------------------------------------------------------------------=
+
+//*============================================================================*
+// MARK: * UMN x Strict Unsigned Integer
+//*============================================================================*
+
+extension UMN {
+    
+    /// A namespace for strict unsigned integer algorithms.
+    ///
+    /// The `base` must be `nonempty` at the start and end of each access.
+    ///
+    /// ```swift
+    /// static func algorithm(_ base: inout Base, input: Input) -> Output
+    /// ```
+    ///
+    /// ### Development
+    ///
+    /// Remaking this as a view when Swift gets view types might be neat.
+    ///
+    @frozen public enum StrictUnsignedInteger<Base> where Base: RandomAccessCollection, Base.Element: UMNSystemInteger & UMNUnsignedInteger {
+        
+        /// The binary integer namespace of this type.
+        public typealias Binary = UMN.StrictBinaryInteger<Base>
+        
+        /// The signed integer namespace of this type.
+        public typealias Signed = UMN.StrictSignedInteger<Base>
+        
+        //*====================================================================*
+        // MARK: * Sub Sequence
+        //*====================================================================*
+        
+        /// The sub sequence namespace of this type.
+        ///
+        /// The `base` may be `empty` at the start and end of each access.
+        ///
+        @frozen public enum SubSequence {
+            
+            /// The binary integer namespace of this type.
+            public typealias Binary = UMN.StrictBinaryInteger<Base>.SubSequence
+            
+            /// The signed integer namespace of this type.
+            public typealias Signed = UMN.StrictSignedInteger<Base>.SubSequence
+        }
+    }
+}
