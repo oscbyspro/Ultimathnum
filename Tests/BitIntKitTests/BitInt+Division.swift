e6144cfabedaa43f6e35df64f8bd4520480a8fb6
Oscar Byström Ericsson <oscbyspro@protonmail.com>
2024-01-09 07:27:42 +0100

    [MainIntKit] MainInt division tests.

--- Tests/BitIntKitTests/BitInt+Division.swift
+++ Tests/BitIntKitTests/BitInt+Division.swift
@@ -25,15 +25,15 @@ extension BitIntTests {
     func testDivision() {
         func whereIsSigned<T>(_ type: T.Type) where T: SystemInteger {
             Test.division( 0 as T,  0 as T,  0 as T,  0 as T, (true,  true ))
-            Test.division(-1 as T,  0 as T, -1 as T, -1 as T, (true,  true ))
             Test.division( 0 as T, -1 as T,  0 as T,  0 as T, (false, false))
+            Test.division(-1 as T,  0 as T, -1 as T, -1 as T, (true,  true ))
             Test.division(-1 as T, -1 as T, -1 as T,  0 as T, (true,  false))
         }
         
         func whereIsUnsigned<T>(_ type: T.Type) where T: SystemInteger {
             Test.division( 0 as T,  0 as T,  0 as T,  0 as T, (true,  true ))
-            Test.division( 1 as T,  0 as T,  1 as T,  1 as T, (true,  true ))
             Test.division( 0 as T,  1 as T,  0 as T,  0 as T, (false, false))
+            Test.division( 1 as T,  0 as T,  1 as T,  1 as T, (true,  true ))
             Test.division( 1 as T,  1 as T,  1 as T,  0 as T, (false, false))
         }
         
---
+++ Tests/MainIntKitTests/MainInt+Division.swift
@@ -0,0 +1,74 @@
+//=----------------------------------------------------------------------------=
+// This source file is part of the Ultimathnum open source project.
+//
+// Copyright (c) 2023 Oscar Byström Ericsson
+// Licensed under Apache License, Version 2.0
+//
+// See http://www.apache.org/licenses/LICENSE-2.0 for license information.
+//=----------------------------------------------------------------------------=
+
+import CoreKit
+import MainIntKit
+import TestKit
+import XCTest
+
+//*============================================================================*
+// MARK: * Main Int x Division
+//*============================================================================*
+
+extension MainIntTests {
+    
+    //=------------------------------------------------------------------------=
+    // MARK: Tests
+    //=------------------------------------------------------------------------=
+    
+    func testDivision() {
+        func whereIsSigned<T>(_ type: T.Type) where T: SystemInteger {
+            Test.division( 7 as T,  3 as T,  2 as T,  1 as T)
+            Test.division( 7 as T, -3 as T, -2 as T,  1 as T)
+            Test.division(-7 as T,  3 as T, -2 as T, -1 as T)
+            Test.division(-7 as T, -3 as T,  2 as T, -1 as T)
+        }
+        
+        func whereIsUnsigned<T>(_ type: T.Type) where T: SystemInteger {
+            Test.division( 7 as T,  0 as T,  7 as T,  7 as T, (true, true))
+            Test.division( 7 as T,  1 as T,  7 as T,  0 as T)
+            Test.division( 7 as T,  2 as T,  3 as T,  1 as T)
+            Test.division( 7 as T,  3 as T,  2 as T,  1 as T)
+        }
+        
+        for type in Self.types {
+            type.isSigned ? whereIsSigned(type) : whereIsUnsigned(type)
+        }
+    }
+    
+    func testDivisionOverflowCases() {
+        func whereIsSigned<T>(_ type: T.Type) where T: SystemInteger {
+            Test.division(-2 as T,  0 as T, -2 as T, -2 as T, (true,  true ))
+            Test.division(-1 as T,  0 as T, -1 as T, -1 as T, (true,  true ))
+            Test.division( 0 as T,  0 as T,  0 as T,  0 as T, (true,  true ))
+            Test.division( 1 as T,  0 as T,  1 as T,  1 as T, (true,  true ))
+            
+            Test.division( T .min, -2 as T,  T(bitPattern: T.Magnitude.msb >> 1),  0 as T, (false,  false))
+            Test.division( T .min, -1 as T,  T(bitPattern: T.Magnitude.msb >> 0),  0 as T, (true,   true )) // FIXME: Only one overflow path...
+            Test.division( T .min,  0 as T,  T(bitPattern: T.Magnitude.msb >> 0),  T .min, (true,   true ))
+            Test.division( T .min,  1 as T,  T(bitPattern: T.Magnitude.msb >> 0),  0 as T, (false,  false))
+            
+            Test.division( T .max, -2 as T,  (T.min + 2) >> 1,  1 as T, (false,  false))
+            Test.division( T .max, -1 as T,  (T.min + 1) >> 0,  0 as T, (false,  false))
+            Test.division( T .max,  0 as T,  (T.max),           T .max, (true,   true ))
+            Test.division( T .max,  1 as T,  (T.max),           0 as T, (false,  false))
+        }
+        
+        func whereIsUnsigned<T>(_ type: T.Type) where T: SystemInteger {
+            Test.division( 0 as T,  0 as T,  0 as T,  0 as T, (true,  true ))
+            Test.division( 1 as T,  0 as T,  1 as T,  1 as T, (true,  true ))
+            Test.division( 2 as T,  0 as T,  2 as T,  2 as T, (true,  true ))
+            Test.division( 3 as T,  0 as T,  3 as T,  3 as T, (true,  true ))
+        }
+        
+        for type in Self.types {
+            type.isSigned ? whereIsSigned(type) : whereIsUnsigned(type)
+        }
+    }
+}
