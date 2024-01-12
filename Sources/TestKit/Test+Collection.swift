08e771116d52556e097be4c7667bdf34e1ef20ae
Oscar Byström Ericsson <oscbyspro@protonmail.com>
2024-01-12 09:17:16 +0100

    [TestKit] Collection.

---
+++ Sources/TestKit/Test+Collection.swift
@@ -0,0 +1,47 @@
+//=----------------------------------------------------------------------------=
+// This source file is part of the Ultimathnum open source project.
+//
+// Copyright (c) 2023 Oscar Byström Ericsson
+// Licensed under Apache License, Version 2.0
+//
+// See http://www.apache.org/licenses/LICENSE-2.0 for license information.
+//=----------------------------------------------------------------------------=
+
+import CoreKit
+import XCTest
+
+//*============================================================================*
+// MARK: * Test x Collection
+//*============================================================================*
+
+extension Test {
+    
+    //=------------------------------------------------------------------------=
+    // MARK: Utilities
+    //=------------------------------------------------------------------------=
+    
+    public static func collection<Element: Equatable>(
+    _ base: some RandomAccessCollection<Element>, _ expectation: [Element],
+    file: StaticString = #file, line: UInt = #line){
+        //=--------------------------------------=
+        for distance in 0 ..< base.count {
+            //=----------------------------------=
+            let index0 = base.index(base.startIndex, offsetBy: distance + 0)
+            let index1 = base.index(base.startIndex, offsetBy: distance + 1)
+            //=----------------------------------=
+            XCTAssertEqual(base[index0], expectation[distance], file: file, line: line)
+            //=----------------------------------=
+            XCTAssertEqual(base.index(before: index1), index0, file: file, line: line)
+            XCTAssertEqual(base.index(after:  index0), index1, file: file, line: line)
+
+            XCTAssertEqual(base.index(base.endIndex, offsetBy: distance + 0 - base.count), index0, file: file, line: line)
+            XCTAssertEqual(base.index(base.endIndex, offsetBy: distance + 1 - base.count), index1, file: file, line: line)
+            //=----------------------------------=
+            XCTAssertEqual(base.distance(from: base.startIndex, to: index0), distance + 0, file: file, line: line)
+            XCTAssertEqual(base.distance(from: base.startIndex, to: index1), distance + 1, file: file, line: line)
+            
+            XCTAssertEqual(base.distance(from: index0, to: base.endIndex), base.count - distance - 0, file: file, line: line)
+            XCTAssertEqual(base.distance(from: index1, to: base.endIndex), base.count - distance - 1, file: file, line: line)
+        }
+    }
+}
--- Tests/ModelsKitTests/Chunked.swift
+++ Tests/ModelsKitTests/Chunked.swift
@@ -37,8 +37,8 @@ final class ChunkedTests: XCTestCase {
         let rhsUnsigned = rhs.map(B.Magnitude.init(bitPattern:))
         //=--------------------------------------=
         func with(isSigned: Bool) {
-            checkElementsEqual(Chunked(lhs,         isSigned: isSigned), rhs,         file: file, line: line)
-            checkElementsEqual(Chunked(lhsUnsigned, isSigned: isSigned), rhsUnsigned, file: file, line: line)
+            Test.collection(Chunked(lhs,         isSigned: isSigned), rhs,         file: file, line: line)
+            Test.collection(Chunked(lhsUnsigned, isSigned: isSigned), rhsUnsigned, file: file, line: line)
         }
         //=--------------------------------------=
         if  isSigned == nil || isSigned == true {
@@ -46,39 +46,6 @@ final class ChunkedTests: XCTestCase {
         }
         if  isSigned == nil || isSigned == false {
             with(isSigned: false)
-        }
-    }
-    
-    func checkElementsEqual<Base: RandomAccessCollection>(
-    _ base: Base, _ expectation: [Base.Element],
-    file: StaticString = #file, line: UInt = #line) where Base.Element: Equatable {
-        //=--------------------------------------=
-        XCTAssertEqual(Array(base), expectation, file: file,  line: line)
-        XCTAssertEqual(Array(base.indices.map({ base[$0] })), expectation, file: file, line: line)
-        //=--------------------------------------=
-        for distance in 0 ..< base.count {
-            //=----------------------------------=
-            let index0 = base.index(base.startIndex, offsetBy: distance + 0)
-            let index1 = base.index(base.startIndex, offsetBy: distance + 1)
-            //=----------------------------------=
-            XCTAssertEqual(base[index0],expectation[distance], file: file, line: line)
-            //=----------------------------------=
-            XCTAssertEqual(base.index(before: index1), index0, file: file, line: line)
-            XCTAssertEqual(base.index(after:  index0), index1, file: file, line: line)
-
-            XCTAssertEqual(base.index(base.endIndex, offsetBy: distance + 0 - base.count), index0, file: file, line: line)
-            XCTAssertEqual(base.index(base.endIndex, offsetBy: distance + 1 - base.count), index1, file: file, line: line)
-            //=----------------------------------=
-            XCTAssertEqual(base.distance(from: base.startIndex, to: index0), distance + 0, file: file, line: line)
-            XCTAssertEqual(base.distance(from: base.startIndex, to: index1), distance + 1, file: file, line: line)
-            
-            XCTAssertEqual(base.distance(from: index0, to: base.endIndex), base.count - distance - 0, file: file, line: line)
-            XCTAssertEqual(base.distance(from: index1, to: base.endIndex), base.count - distance - 1, file: file, line: line)
-        }
-        //=--------------------------------------=
-        for distance in 0 ... base.count + 1 {
-            XCTAssert(base.prefix(distance).elementsEqual(expectation.prefix(distance)), file: file, line: line)
-            XCTAssert(base.suffix(distance).elementsEqual(expectation.suffix(distance)), file: file, line: line)
         }
     }
 }
