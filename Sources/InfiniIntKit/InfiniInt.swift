f5f0e25306933e4ecd1c8492547e6ba49dbeb448
Oscar Byström Ericsson <oscbyspro@protonmail.com>
2024-01-19 13:50:39 +0100

    [InfiniIntKit] Skeleton.

---
+++ Sources/InfiniIntKit/InfiniInt.swift
@@ -0,0 +1,146 @@
+//=----------------------------------------------------------------------------=
+// This source file is part of the Ultimathnum open source project.
+//
+// Copyright (c) 2023 Oscar Byström Ericsson
+// Licensed under Apache License, Version 2.0
+//
+// See http://www.apache.org/licenses/LICENSE-2.0 for license information.
+//=----------------------------------------------------------------------------=
+
+import CoreKit
+
+//*============================================================================*
+// MARK: * Infini Int
+//*============================================================================*
+
+/// ### Development
+///
+/// Consider making the storage model the same model that is used by init(load:)
+/// and perhaps words. This would maximize interoperability.
+///
+@frozen public struct InfiniInt {
+    
+    public typealias IntegerLiteralType = StaticBigInt
+    
+    public typealias Words = ContiguousArray<UX>
+    
+    //=------------------------------------------------------------------------=
+    // MARK: Meta Data
+    //=------------------------------------------------------------------------=
+    
+    @inlinable public static var bitWidth: Magnitude {
+        Magnitude.bitWidth
+    }
+    
+    //=------------------------------------------------------------------------=
+    // MARK: State
+    //=------------------------------------------------------------------------=
+    
+    @usableFromInline var storage: Storage
+    
+    //=------------------------------------------------------------------------=
+    // MARK: Initializers
+    //=------------------------------------------------------------------------=
+    
+    @inlinable init(unchecked storage: consuming Storage) {
+        self.storage = storage
+        Swift.assert(self.storage.isNormal)
+    }
+    
+    @inlinable init(normalizing storage: consuming Storage) {
+        self.storage = storage
+        self.storage.normalize()
+    }
+    
+    //*========================================================================*
+    // MARK: * Magnitude
+    //*========================================================================*
+    
+    @frozen public struct Magnitude {
+                
+        public typealias IntegerLiteralType = StaticBigInt
+        
+        public typealias Words = ContiguousArray<UX>
+        
+        //=--------------------------------------------------------------------=
+        // MARK: Meta Data
+        //=--------------------------------------------------------------------=
+        
+        @inlinable public static var bitWidth: Magnitude {
+            fatalError("TODO")
+        }
+        
+        //=--------------------------------------------------------------------=
+        // MARK: State
+        //=--------------------------------------------------------------------=
+        
+        @usableFromInline var storage: Storage
+        
+        //=--------------------------------------------------------------------=
+        // MARK: Initializers
+        //=--------------------------------------------------------------------=
+        
+        @inlinable init(unchecked storage: consuming Storage) {
+            self.storage = storage
+            Swift.assert(self.storage.isNormal)
+        }
+        
+        @inlinable init(normalizing storage: consuming Storage) {
+            self.storage = storage
+            self.storage.normalize()
+        }
+    }
+    
+    //*========================================================================*
+    // MARK: * Storage
+    //*========================================================================*
+    
+    /// ### Development
+    ///
+    /// - TODO: Consider short array optimization based on enum.
+    ///
+    /// - TODO: Consider short array optimization based on emptiness.
+    ///
+    @frozen @usableFromInline struct Storage: Hashable {
+        
+        @usableFromInline typealias Body = ContiguousArray<UX>
+        @usableFromInline typealias Tail = Body.Element
+        
+        //=--------------------------------------------------------------------=
+        // MARK: State
+        //=--------------------------------------------------------------------=
+        
+        @usableFromInline var body: Body
+        @usableFromInline var tail: Tail
+        
+        //=--------------------------------------------------------------------=
+        // MARK: Initializers
+        //=--------------------------------------------------------------------=
+        
+        @inlinable init(body: Body, tail: Tail) {
+            self.body = body
+            self.tail = tail
+        }
+        
+        //=--------------------------------------------------------------------=
+        // MARK: Utilities
+        //=--------------------------------------------------------------------=
+        
+        @inlinable var isNormal: Bool {
+            self.body.last != self.tail
+        }
+        
+        @inlinable mutating func normalize() {
+            while self.body.last == self.tail {
+                ((self.body)).removeLast()
+            }
+        }
+    }
+}
+
+//=----------------------------------------------------------------------------=
+// MARK: + Aliases
+//=----------------------------------------------------------------------------=
+
+public typealias IXL = InfiniInt
+public typealias UXL = InfiniInt.Magnitude
