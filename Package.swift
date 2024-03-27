5104af58126790c69380a4ba2bf0c6501bb7ac8b
Oscar Byström Ericsson <oscbyspro@protonmail.com>
2024-03-27 18:41:28 +0100

    [LICENSE] Cleanup.

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 //=----------------------------------------------------------------------------=
 // This source file is part of the Ultimathnum open source project.
 //
-// Copyright (c) 2023 Oscar Byström Ericsson
+// Copyright (c)  2023 Oscar Byström Ericsson
 // Licensed under Apache License, Version 2.0
 //
 // See http://www.apache.org/licenses/LICENSE-2.0 for license information.
