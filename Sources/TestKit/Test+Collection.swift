e5b116c49eb187d66aea88dd9866344426065a54
Oscar Byström Ericsson <oscbyspro@protonmail.com>
2024-03-30 07:37:44 +0100

    [TestKit] Cleanup of method signatures in Test+Collection.swift.

--- Sources/TestKit/Test+Collection.swift
+++ Sources/TestKit/Test+Collection.swift
@@ -20,23 +20,24 @@ extension Test {
     // MARK: Utilities
     //=------------------------------------------------------------------------=
     
-    public static func collection<Element: Equatable>(
-    _ base: some RandomAccessCollection<Element>, _ expectation: [Element],
-    file: StaticString = #file, line: UInt = #line){
-        //=--------------------------------------=
+    public static func collection<Element>(
+        _ base: some RandomAccessCollection<Element>, 
+        _ expectation: [Element],
+        file: StaticString = #file, 
+        line: UInt = #line
+    )   where Element: Equatable {
         for distance in 0 ..< base.count {
-            //=----------------------------------=
             let index0 = base.index(base.startIndex, offsetBy: distance + 0)
             let index1 = base.index(base.startIndex, offsetBy: distance + 1)
-            //=----------------------------------=
+            
             XCTAssertEqual(base[index0], expectation[distance], file: file, line: line)
-            //=----------------------------------=
+            
             XCTAssertEqual(base.index(before: index1), index0, file: file, line: line)
             XCTAssertEqual(base.index(after:  index0), index1, file: file, line: line)
 
             XCTAssertEqual(base.index(base.endIndex, offsetBy: distance + 0 - base.count), index0, file: file, line: line)
             XCTAssertEqual(base.index(base.endIndex, offsetBy: distance + 1 - base.count), index1, file: file, line: line)
-            //=----------------------------------=
+            
             XCTAssertEqual(base.distance(from: base.startIndex, to: index0), distance + 0, file: file, line: line)
             XCTAssertEqual(base.distance(from: base.startIndex, to: index1), distance + 1, file: file, line: line)
             
