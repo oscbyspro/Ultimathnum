715bd9b2009de46cbe3bb7e2f4d136fb07aaf6b9
Oscar Byström Ericsson <oscbyspro@protonmail.com>
2024-03-30 08:22:09 +0100

    [TestKit] Cleanup of method signatures in Test+Logic.swift.

--- Sources/TestKit/Test+Logic.swift
+++ Sources/TestKit/Test+Logic.swift
@@ -20,44 +20,59 @@ extension Test {
     // MARK: Utilities
     //=------------------------------------------------------------------------=
     
-    public static func not<T: BitOperable & Equatable>(
-    _ instance: T, _ value: T,
-    file: StaticString = #file, line: UInt = #line) {
-        //=--------------------------------------=
-        XCTAssertEqual(~instance, value, file: file, line: line)
-        XCTAssertEqual(~value, instance, file: file, line: line)
+    public static func not<T>(
+        _ instance: T, 
+        _ expectation: T,
+        file: StaticString = #file,
+        line: UInt = #line
+    )   where T: BitOperable & Equatable {
+        
+        XCTAssertEqual(~instance, expectation, file: file, line: line)
+        XCTAssertEqual(~expectation, instance, file: file, line: line)
     }
     
-    public static func and<T: BitOperable & Equatable>(
-    _ lhs: T, _ rhs: T, _ value: T,
-    file: StaticString = #file, line: UInt = #line) {
-        //=--------------------------------------=
-        XCTAssertEqual(lhs & rhs, value, file: file, line: line)
-        XCTAssertEqual(rhs & lhs, value, file: file, line: line)
-        //=--------------------------------------=
-        XCTAssertEqual({ var x = lhs; x &= rhs; return x }(), value, file: file, line: line)
-        XCTAssertEqual({ var x = rhs; x &= lhs; return x }(), value, file: file, line: line)
+    public static func and<T>(
+        _ lhs: T, 
+        _ rhs: T,
+        _ expectation: T,
+        file: StaticString = #file,
+        line: UInt = #line
+    )   where T: BitOperable & Equatable {
+        
+        XCTAssertEqual(lhs & rhs, expectation, file: file, line: line)
+        XCTAssertEqual(rhs & lhs, expectation, file: file, line: line)
+        
+        XCTAssertEqual({ var x = lhs; x &= rhs; return x }(), expectation, file: file, line: line)
+        XCTAssertEqual({ var x = rhs; x &= lhs; return x }(), expectation, file: file, line: line)
     }
     
-    public static func or<T: BitOperable & Equatable>(
-    _ lhs: T, _ rhs: T, _ value: T,
-    file: StaticString = #file, line: UInt = #line) {
-        //=--------------------------------------=
-        XCTAssertEqual(lhs | rhs, value, file: file, line: line)
-        XCTAssertEqual(rhs | lhs, value, file: file, line: line)
-        //=--------------------------------------=
-        XCTAssertEqual({ var x = lhs; x |= rhs; return x }(), value, file: file, line: line)
-        XCTAssertEqual({ var x = rhs; x |= lhs; return x }(), value, file: file, line: line)
+    public static func or<T>(
+        _ lhs: T,
+        _ rhs: T,
+        _ expectation: T,
+        file: StaticString = #file,
+        line: UInt = #line
+    )   where T: BitOperable & Equatable {
+        
+        XCTAssertEqual(lhs | rhs, expectation, file: file, line: line)
+        XCTAssertEqual(rhs | lhs, expectation, file: file, line: line)
+        
+        XCTAssertEqual({ var x = lhs; x |= rhs; return x }(), expectation, file: file, line: line)
+        XCTAssertEqual({ var x = rhs; x |= lhs; return x }(), expectation, file: file, line: line)
     }
     
-    public static func xor<T: BitOperable & Equatable>(
-    _ lhs: T, _ rhs: T, _ value: T,
-    file: StaticString = #file, line: UInt = #line) {
-        //=--------------------------------------=
-        XCTAssertEqual(lhs ^ rhs, value, file: file, line: line)
-        XCTAssertEqual(rhs ^ lhs, value, file: file, line: line)
-        //=--------------------------------------=
-        XCTAssertEqual({ var x = lhs; x ^= rhs; return x }(), value, file: file, line: line)
-        XCTAssertEqual({ var x = rhs; x ^= lhs; return x }(), value, file: file, line: line)
+    public static func xor<T>(
+        _ lhs: T,
+        _ rhs: T,
+        _ expectation: T,
+        file: StaticString = #file,
+        line: UInt = #line
+    )   where T: BitOperable & Equatable {
+        
+        XCTAssertEqual(lhs ^ rhs, expectation, file: file, line: line)
+        XCTAssertEqual(rhs ^ lhs, expectation, file: file, line: line)
+        
+        XCTAssertEqual({ var x = lhs; x ^= rhs; return x }(), expectation, file: file, line: line)
+        XCTAssertEqual({ var x = rhs; x ^= lhs; return x }(), expectation, file: file, line: line)
     }
 }
