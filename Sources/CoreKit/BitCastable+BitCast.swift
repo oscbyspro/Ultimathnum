1fa27787c4ad4eb558cd5dbdaa9ac3356a860e17
Oscar Byström Ericsson <oscbyspro@protonmail.com>
2024-04-28 08:21:27 +0200

    Added: BitCastable/subscript(raw:).

--- Sources/CoreKit/BitCastable+BitCast.swift
+++ Sources/CoreKit/BitCastable+BitCast.swift
@@ -8,7 +8,7 @@
 //=----------------------------------------------------------------------------=
 
 //*============================================================================*
-// MARK: * Bit Castable x Bit
+// MARK: * Bit Castable + Bit Cast
 //*============================================================================*
 
 extension BitCastable {
@@ -20,5 +20,29 @@ extension BitCastable {
     @inline(__always) //  performance: please fold it like a paper airplane
     @inlinable public init(raw source: consuming some BitCastable<BitPattern>) {
         self.init(raw: source.load(as: BitPattern.self))
+    }
+    
+    //=------------------------------------------------------------------------=
+    // MARK: Transformations
+    //=------------------------------------------------------------------------=
+    
+    /// Reinterprets this instance as an instance of type `T`.
+    ///
+    /// ```swift
+    /// var value = IX(0)
+    /// var error = value[raw: UX.self][{ $0.decremented() }]
+    ///
+    /// print(value) // -1
+    /// print(error) // true
+    /// ```
+    ///
+    @inlinable public subscript<T>(raw type: T.Type) -> T where T: BitCastable<BitPattern> {
+        consuming get {
+            T(raw: self)
+        }
+        
+        mutating set {
+            self = Self(raw: newValue)
+        }
     }
 }
