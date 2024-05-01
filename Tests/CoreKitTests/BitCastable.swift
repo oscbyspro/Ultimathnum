54fd14996eb6679f7d803ce2f752af6346a0ccc0
Oscar Byström Ericsson <oscbyspro@protonmail.com>
2024-05-01 03:27:52 +0200

    [Tests] BitCastable.

---
+++ Tests/CoreKitTests/BitCastable.swift
@@ -0,0 +1,66 @@
+//=----------------------------------------------------------------------------=
+// This source file is part of the Ultimathnum open source project.
+//
+// Copyright (c)  2023 Oscar Byström Ericsson
+// Licensed under Apache License, Version 2.0
+//
+// See http://www.apache.org/licenses/LICENSE-2.0 for license information.
+//=----------------------------------------------------------------------------=
+
+import CoreKit
+import TestKit
+
+//*============================================================================*
+// MARK: * Bit Castable
+//*============================================================================*
+
+final class BitCastableTests: XCTestCase {
+    
+    //=------------------------------------------------------------------------=
+    // MARK: Tests
+    //=------------------------------------------------------------------------=
+    
+    func testRawInitUsingCoreSystemsIntegers() {
+        func whereIs<T>(_ type: T.Type) where T: SystemsInteger {
+            typealias S = T.Signitude
+            typealias M = T.Magnitude
+            
+            Test().same(T(raw: S.min),  T.msb)
+            Test().same(T(raw: S.max), ~T.msb)
+            
+            Test().same(T(raw: M.min),  T(repeating: 0))
+            Test().same(T(raw: M.max),  T(repeating: 1))
+        }
+
+        for type in coreSystemsIntegers {
+            whereIs(type)
+        }
+    }
+    
+    func testRawSubscriptUsingCoreSystemsIntegers() {
+        func whereIs<T>(_ type: T.Type) where T: SystemsInteger {
+            typealias S = T.Signitude
+            typealias M = T.Magnitude
+            //=----------------------------------=
+            var value:  T
+            //=----------------------------------=
+            value = T(raw: M.max)
+            
+            Test().same(true,  value[raw: M.self][{ $0.incremented() }])
+            Test().same(value, T(raw: M.min))
+            Test().same(false, value[raw: S.self][{ $0.decremented() }])
+            Test().same(value, T(raw: M.max))
+
+            value = T(raw: S.max)
+            
+            Test().same(true,  value[raw: S.self][{ $0.incremented() }])
+            Test().same(value, T(raw: S.min))
+            Test().same(false, value[raw: M.self][{ $0.decremented() }])
+            Test().same(value, T(raw: S.max))
+        }
+
+        for type in coreSystemsIntegers {
+            whereIs(type)
+        }
+    }
+}
