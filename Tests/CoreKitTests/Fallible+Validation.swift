a8836b222f89d0018cb70efeb3799909983b1082
Oscar Byström Ericsson <oscbyspro@protonmail.com>
2024-05-14 06:49:02 +0200

    [Tests] Veto.

--- Tests/CoreKitTests/Fallible+Validation.swift
+++ Tests/CoreKitTests/Fallible+Validation.swift
@@ -35,15 +35,21 @@ extension FallibleTests {
     }
     
     func testInvalidation() {
+        Test().same(IX(0).veto(              ), Fallible(0, error: true ))
+        Test().same(IX(1).veto(       false  ), Fallible(1, error: false))
+        Test().same(IX(2).veto(       true   ), Fallible(2, error: true ))
+        Test().same(IX(3).veto({ _ in false }), Fallible(3, error: false))
+        Test().same(IX(4).veto({ _ in true  }), Fallible(4, error: true ))
+        
         Test().same(Fallible<IX>(0, error: false).veto(              ), Fallible(0, error: true ))
-        Test().same(Fallible<IX>(0, error: true ).veto(              ), Fallible(0, error: true ))
-        Test().same(Fallible<IX>(0, error: false).veto(       false  ), Fallible(0, error: false))
-        Test().same(Fallible<IX>(0, error: false).veto(       true   ), Fallible(0, error: true ))
-        Test().same(Fallible<IX>(0, error: true ).veto(       false  ), Fallible(0, error: true ))
-        Test().same(Fallible<IX>(0, error: true ).veto(       true   ), Fallible(0, error: true ))
-        Test().same(Fallible<IX>(0, error: false).veto({ _ in false }), Fallible(0, error: false))
-        Test().same(Fallible<IX>(0, error: false).veto({ _ in true  }), Fallible(0, error: true ))
-        Test().same(Fallible<IX>(0, error: true ).veto({ _ in false }), Fallible(0, error: true ))
-        Test().same(Fallible<IX>(0, error: true ).veto({ _ in true  }), Fallible(0, error: true ))
+        Test().same(Fallible<IX>(1, error: true ).veto(              ), Fallible(1, error: true ))
+        Test().same(Fallible<IX>(2, error: false).veto(       false  ), Fallible(2, error: false))
+        Test().same(Fallible<IX>(3, error: false).veto(       true   ), Fallible(3, error: true ))
+        Test().same(Fallible<IX>(4, error: true ).veto(       false  ), Fallible(4, error: true ))
+        Test().same(Fallible<IX>(5, error: true ).veto(       true   ), Fallible(5, error: true ))
+        Test().same(Fallible<IX>(6, error: false).veto({ _ in false }), Fallible(6, error: false))
+        Test().same(Fallible<IX>(7, error: false).veto({ _ in true  }), Fallible(7, error: true ))
+        Test().same(Fallible<IX>(8, error: true ).veto({ _ in false }), Fallible(8, error: true ))
+        Test().same(Fallible<IX>(9, error: true ).veto({ _ in true  }), Fallible(9, error: true ))
     }
 }
